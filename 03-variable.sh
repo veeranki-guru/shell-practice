26bac7d8444ab394717790030f4859a4fceba4fb
Veeranki-guru <9885850326guru@gmail.com>
2026-05-28 11:07:14 +0530

    02-conversations

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1="$PERSON1"
+PERSON2="$PERSON2"
+
+echo "$PERSON1:: Hi $PERSON2 how are you"
+echo "$PERSON2:: Hi i am fine $PERSON1"
+echo "$PERSON1:: $PERSON2 you learning shell-script right"
+echo "$PERSON2:: Yes $PERSON1"
