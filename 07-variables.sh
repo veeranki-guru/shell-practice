eeede1889ba6c2ef64f1a3f206b512a7a2c8798c
Veeranki-guru <9885850326guru@gmail.com>
2026-05-28 23:19:18 +0530

    conversations

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#DATA=$(date)
-
 START_TIME=$(date +%s)
 
 sleep 10
 
-END_TIME=$(data +%s)
+END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 
-echo "script excuted in: $TOTAL_TIME seconds"
+echo "script executed in: $TOTAL_TIME seconds"
