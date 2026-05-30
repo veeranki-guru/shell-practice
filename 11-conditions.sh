5f5f2195cf6afdcbb80ff79fe82ccf7debe98636
Veeranki-guru <9885850326guru@gmail.com>
2026-05-30 10:05:16 +0530

    conversations

---
+++ 11-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter the number:"
+read NUMBER
+
+if [ $(($NUMBER % 2)) -eq 0 ]; then
+   echo "Given number is $NUMBER is EVEN"
+eles
+   echo "Given number is $NUMBER is odd"   
+fi   
