e2fa24ffea8dd1945ce9f3be4492073a661e04d4
Veeranki-guru <9885850326guru@gmail.com>
2026-05-30 09:37:43 +0530

    conversations

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -7,7 +7,7 @@ if [ $NUMBER -lt 0 ]; then
 elif [ $NUMBER -eq 10 ]; then
    echo "Given number $NUMBER is equal to 10"   
 else
-   echo "Given number $NUMBER is greater than or equal to 0"
+   echo "Given number $NUMBER is greater than  to 0"
 fi
 
 # -gt  => greater than
