b63f64bd7edc8dad8e00f27c7c2dbe4a23b2d0bc
Veeranki-guru <9885850326guru@gmail.com>
2026-05-30 09:18:49 +0530

    conversations

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -3,12 +3,11 @@
 NUMBER=$1
 
 if [ $NUMBER -lt 0 ]; then
-   echo "Given Number $NUMBER is  less then 10" 
-eles
-   echo "Given number $NUMBER is greater then are equal to 10"
+   echo "Given number $NUMBER is less than 0"
+else
+   echo "Given number $NUMBER is greater than or equal to 0"
 fi
 
-#-gt
-#-eq
-#-ne
-
+# -gt  => greater than
+# -eq  => equal to
+# -ne  => not equal
