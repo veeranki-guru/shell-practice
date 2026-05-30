6d2eaca52c8f7b9e8a073f325d0d24ee4fe68d49
Veeranki-guru <9885850326guru@gmail.com>
2026-05-30 11:32:34 +0530

    conversations

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,6 +4,8 @@ USERID=$( id -u) #check of root user
 
 if [ $USERID -ne 0 ]; then
    echo "ERROR:: please run this script with root privelege"
+   exit 1
+
 fi   #user id -u ;1000 == 0 it error
 
 dnf install mysql -y
@@ -11,6 +13,7 @@ dnf install mysql -y
 if [ $? -ne 0 ]; then
    echo "ERROR:: installing mysql is failure"
    exit 1 # it faile it will no process and stop
+   
 else
    echo "installing mysql is SUCCES" 
 fi #$? meaning 1000 == 0 
