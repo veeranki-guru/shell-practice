9fbf9dfd044444bedf7396ca274b44b0d6054b36
Veeranki-guru <9885850326guru@gmail.com>
2026-05-30 09:15:10 +0530

    conversations

---
+++ 10-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -lt 0 ]; then
+   echo "Given Number $NUMBER is  less then 10" 
+eles
+   echo "Given number $NUMBER is greater then are equal to 10"
+fi
+
+#-gt
+#-eq
+#-ne
+
